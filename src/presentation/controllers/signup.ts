d9e27604c0ce4b1319e6270aba1d972302ce61ae
Tiago Gomes <tidgomes@gmail.com>
2023-11-07 11:15:28 -0300

    feat: add custom error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,14 @@
-import type { HttpRequest, HttpResponse } from '../protocols/http.ts'
+import type { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { body: new Error('Missing param: name'), statusCode: 400 }
+      return { body: new MissingParamError('name'), statusCode: 400 }
     }
     if (!httpRequest.body.email) {
       return {
-        body: new Error('Missing param: email'), statusCode: 400
+        body: new MissingParamError('email'), statusCode: 400
       }
     }
   }
