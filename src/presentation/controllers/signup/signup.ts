cfbc39fac3ce5a106c32bfda2b3d78833628d1c7
Tiago Gomes <tidgomes@gmail.com>
2024-01-08 12:07:01 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -31,6 +31,7 @@ export class SignUpController implements Controller {
       const account = await this.addAccount.add({ name, email, password })
       return ok(account)
     } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
