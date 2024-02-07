82d09df0d57654ff2ae7e33c4b2f50278def8d5a
Tiago Gomes <tidgomes@gmail.com>
2024-02-07 17:01:21 -0300

    chore: update where coverage is collected from

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: '@shelf/jest-mongodb',
