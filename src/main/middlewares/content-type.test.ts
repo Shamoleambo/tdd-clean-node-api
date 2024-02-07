dfd87b752692d70eab641c0bcbe959d8aa2ebeed
Tiago Gomes <tidgomes@gmail.com>
2024-02-07 12:22:25 -0300

    test: ensure response returns xml content-type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,12 +3,23 @@ import app from '../config/app'
 
 describe('Content-Type Middleware', () => {
   test('should return default Content-Type as json', async () => {
-    app.get('/test_content_type', (req, res) => {
+    app.get('/test_content_type_json', (req, res) => {
       res.send()
     })
 
     await request(app)
-      .get('/test_content_type')
+      .get('/test_content_type_json')
       .expect('Content-Type', /json/)
+  })
+
+  test('should return xml when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('Content-Type', /xml/)
   })
 })
