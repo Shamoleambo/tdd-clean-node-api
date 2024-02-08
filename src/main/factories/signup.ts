be3cd05222136da5896eaee5e5a1cb885b084985
Tiago Gomes <tidgomes@gmail.com>
2024-02-08 08:58:57 -0300

    feat: ensure LogController Decorator calls handle method from controller

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,14 +1,17 @@
+import { Controller } from '../../presentation/protocols'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignupController = (): SignUpController => {
+export const makeSignupController = (): Controller => {
   const SALT = 12
   const accountRepository = new AccountMongoRepository()
   const bcryptAdapter = new BcryptAdapter(SALT)
   const addAccount = new DbAddAccount(bcryptAdapter, accountRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  return new SignUpController(emailValidatorAdapter, addAccount)
+  const signupController = new SignUpController(emailValidatorAdapter, addAccount)
+  return new LogControllerDecorator(signupController)
 }
