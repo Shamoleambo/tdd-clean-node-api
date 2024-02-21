51ac9606263b38f63ae07d9cc71a8a7eee716f00
Tiago Gomes <tidgomes@gmail.com>
2024-02-21 09:40:42 -0300

    refactor: move Login dependencies to a single file

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,8 +1,7 @@
-import { Authentication } from '../../../domain/usecases/authentication'
+import { LoginController } from './login'
+import { HttpRequest, EmailValidator, Authentication } from './login-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
-import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
-import { LoginController } from './login'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
