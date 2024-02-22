ae24fdc1bd193faa489e97fd8a5d6748376f438b
Tiago Gomes <tidgomes@gmail.com>
2024-02-22 10:05:38 -0300

    test: ensure RequiredField Validation do not return if validation succeed

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toEqual(new MissingParamError('required_field'))
   })
+
+  test('should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('required_field')
+    const error = sut.validate({ required_field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
