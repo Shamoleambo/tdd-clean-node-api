f1530e87ffa0ac985a0eea01744b97479e183ae2
Tiago Gomes <tidgomes@gmail.com>
2024-02-29 09:28:21 -0300

    test: ensure 200 is returned on login

--- src/main/routes/login-route.test.ts
+++ src/main/routes/login-route.test.ts
@@ -1,6 +1,10 @@
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -8,8 +12,8 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accounts = await MongoHelper.getCollection('accounts')
-    await accounts.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -25,6 +29,24 @@ describe('Login Routes', () => {
           email: 'any_email@mail.com',
           password: 'any_password',
           passwordConfirmation: 'any_password'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: '123'
         })
         .expect(200)
     })
